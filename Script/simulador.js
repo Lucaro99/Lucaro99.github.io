37fe7ec502e9424b7f262c912f777d72e7512016
Lucaro99 <rcata99@gmail.com>
2023-07-05 16:56:49 -0500

    simlador-js

--- Script/simulador.js
+++ Script/simulador.js
@@ -1,101 +1,67 @@
-const f = new Intl.NumberFormat("es-us", {
-  style: "currency",
-  maximumFractionDigits: 0,
-  currency: "USD",
+const formatCurrency = (value) => {
+  return new Intl.NumberFormat('es-CO', {
+    style: 'currency',
+    maximumFractionDigits: 0,
+    currency: 'COP'
+  }).format(value);
+};
+
+const rangeValor = document.getElementById('rangoValor');
+const rangeTiempo = document.getElementById('rangoTiempo');
+const rangeVValor = document.getElementById('rangeV');
+const rangeVTiempo = document.getElementById('rangeV2');
+
+const setValueValor = () => {
+  const newValue = Number((rangeValor.value - rangeValor.min) * 100 / (rangeValor.max - rangeValor.min));
+  const newPosition = 10 - (newValue * 0.2);
+  rangeVValor.innerHTML = `<span>${formatCurrency(rangeValor.value)}</span>`;
+  rangeVValor.style.left = `calc(${newValue}% + (${newPosition}px))`;
+};
+
+const setValueTiempo = () => {
+  const newValue = Number((rangeTiempo.value - rangeTiempo.min) * 100 / (rangeTiempo.max - rangeTiempo.min));
+  const newPosition = 10 - (newValue * 0.2);
+  rangeVTiempo.innerHTML = `<span>${rangeTiempo.value} meses </span>`;
+  rangeVTiempo.style.left = `calc(${newValue}% + (${newPosition}px))`;
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  setValueValor();
+  setValueTiempo();
 });
 
-var sliderValor = document.getElementById("rangoValor");
-var sliderTiempo = document.getElementById("rangoTiempo");
+rangeValor.addEventListener('input', setValueValor);
+rangeTiempo.addEventListener('input', setValueTiempo);
+
+////
+
 var outputValor = document.getElementById("valor");
-var outputValor1 = document.getElementById("valor1");
-var outputTiempo = document.getElementById("tiempo");
-var outputCuota = document.getElementById("cuota");
+var sliderTiempo = document.getElementById("rangeV2");
+var sliderValor = document.getElementById("rangeV");
 var outputIntereses = document.getElementById("intereses");
+var outputAval = document.getElementById("aval");
+var outputCuota = document.getElementById("cuota");
 var outputTotal = document.getElementById("total");
 var tasa = 0.03;
 
-outputTiempo.innerHTML = sliderTiempo.value;
+
 outputValor.innerHTML = f.format(sliderValor.value);
-outputValor1.innerHTML = f.format(sliderValor.value);
+outputTiempo.innerHTML = sliderTiempo.value;
 
+alert ("arepa");
 //Evento Cambio de Valor en Spans
-
-
-sliderValor.oninput = function () {
-
-  outputValor1.innerHTML = f.format(this.value);
-  outputCuota.innerHTML = f.format(
-    (tasa * sliderValor.value) /
-      (1 - Math.pow(1 + tasa, -1 * sliderTiempo.value))
-  );
-  outputValor.innerHTML = f.format(this.value);
-  outputCuota.innerHTML = f.format(
-    (tasa * sliderValor.value) /
-      (1 - Math.pow(1 + tasa, -1 * sliderTiempo.value))
-  );
-  outputIntereses.innerHTML = f.format(
-    ((tasa * sliderValor.value) /
-      (1 - Math.pow(1 + tasa, -1 * sliderTiempo.value))) *
-      sliderTiempo.value -
-      sliderValor.value
-  );
-  outputTotal.innerHTML = f.format(
-    ((tasa * sliderValor.value) /
-      (1 - Math.pow(1 + tasa, -1 * sliderTiempo.value))) *
-      sliderTiempo.value
-  );
-};
-
-sliderTiempo.oninput = function () {
-  outputTiempo.innerHTML = this.value;
-  outputCuota.innerHTML = f.format(
-    (tasa * sliderValor.value) /
-      (1 - Math.pow(1 + tasa, -1 * sliderTiempo.value))
-  );
-  outputIntereses.innerHTML = f.format(
-    ((tasa * sliderValor.value) /
-      (1 - Math.pow(1 + tasa, -1 * sliderTiempo.value))) *
-      sliderTiempo.value -
-      sliderValor.value
-  );
-  outputTotal.innerHTML = f.format(
-    ((tasa * sliderValor.value) /
-      (1 - Math.pow(1 + tasa, -1 * sliderTiempo.value))) *
-      sliderTiempo.value
-  );
-};
-
-//Evento Cambio Color Slider
-sliderValor.addEventListener("mousemove", function () {
-  var ejeValor = sliderValor.value / 1000000;
-  var colorValor =
-    "linear-gradient(90deg, rgb(237,30,121)" +
-    ejeValor +
-    "%, rgb(214,214,214)" +
-    ejeValor +
-    "%)";
-  sliderValor.style.background = colorValor;
-});
-
-sliderTiempo.addEventListener("mousemove", function () {
-  var ejeTiempo = sliderTiempo.value * (100 / 48) - 3;
-  var colorTiempo =
-    "linear-gradient(90deg, rgb(237,30,121)" +
-    ejeTiempo +
-    "%, rgb(214,214,214)" +
-    ejeTiempo +
-    "%)";
-  sliderTiempo.style.background = colorTiempo;
-});
-
-var elInput = document.querySelector('#rangoValor');
-if (elInput) {
-  var etiqueta = document.querySelector('#etiqueta');
-  if (etiqueta) {
-    etiqueta.innerHTML = elInput.value;
-
-    elInput.addEventListener('input', function() {
-      etiqueta.innerHTML = elInput.value;
-    }, false);
-  }
+sliderValor.oninput = function() {
+	outputValor.innerHTML= f.format(this.value);
+  outputCuota.innerHTML= f.format((tasa * sliderValor.value) / (1 - Math.pow(1+(tasa), -1 * sliderTiempo.value)));
+  outputIntereses.innerHTML= f.format(((tasa * sliderValor.value) / (1 - Math.pow(1+(tasa), -1 * sliderTiempo.value))*sliderTiempo.value) - sliderValor.value);
+  outputTotal.innerHTML= f.format(((tasa * sliderValor.value) / (1 - Math.pow(1+(tasa), -1 * sliderTiempo.value))*sliderTiempo.value));
 }
+
+sliderTiempo.oninput = function() {
+	outputTiempo.innerHTML= this.value;
+	outputCuota.innerHTML= f.format((tasa * sliderValor.value) / (1 - Math.pow(1+(tasa), -1 * sliderTiempo.value)));
+  outputIntereses.innerHTML= f.format(((tasa * sliderValor.value) / (1 - Math.pow(1+(tasa), -1 * sliderTiempo.value))*sliderTiempo.value) - sliderValor.value);
+  outputTotal.innerHTML= f.format(((tasa * sliderValor.value) / (1 - Math.pow(1+(tasa), -1 * sliderTiempo.value))*sliderTiempo.value));
+}
+
+
